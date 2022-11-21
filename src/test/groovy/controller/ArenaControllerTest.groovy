6967a8470885fde430acd8bfc0b577c06271ee8e
Ian <ianbeltrao@gmail.com>
2022-11-21 21:27:00 +0000

    Fixed arena controller test

--- src/test/groovy/controller/ArenaControllerTest.groovy
+++ src/test/groovy/controller/ArenaControllerTest.groovy
@@ -1,68 +1,139 @@
-package com.RafaelNTeixeira.projeto.controller.game;
+package controller
 
-import com.RafaelNTeixeira.projeto.Game;
-import com.RafaelNTeixeira.projeto.Graphics.GUI;
-import com.RafaelNTeixeira.projeto.States.LoseState;
-import com.RafaelNTeixeira.projeto.States.MenuState;
-import com.RafaelNTeixeira.projeto.States.PauseState;
-import com.RafaelNTeixeira.projeto.model.game.arena.Arena;
-import com.RafaelNTeixeira.projeto.model.menu.Lose;
-import com.RafaelNTeixeira.projeto.model.menu.Menu;
-import com.RafaelNTeixeira.projeto.model.menu.Pause;
-import com.googlecode.lanterna.input.KeyStroke;
-import com.googlecode.lanterna.input.KeyType;
+import com.RafaelNTeixeira.projeto.Game
+import com.RafaelNTeixeira.projeto.Graphics.GUI
+import com.RafaelNTeixeira.projeto.States.MenuState
+import com.RafaelNTeixeira.projeto.controller.game.ArenaController
+import com.RafaelNTeixeira.projeto.controller.game.EnemyController
+import com.RafaelNTeixeira.projeto.controller.game.HeroController
+import com.RafaelNTeixeira.projeto.model.game.arena.Arena
+import com.RafaelNTeixeira.projeto.model.game.elements.Hero
+import com.RafaelNTeixeira.projeto.model.menu.Menu
+import com.googlecode.lanterna.input.KeyStroke
+import com.googlecode.lanterna.input.KeyType
+import org.mockito.Mockito
+import spock.lang.Specification
 
-import java.io.IOException;
+class ArenaControllerTest extends Specification{
+    private def arena
+    private def Acontrol
+    private def game
+    private def key
+    private def time
+    private def arenaController
+    private def e_c
+    private def h_c
 
-public class ArenaController extends GameController {
-    private HeroController heroController;
-    private EnemyController EnemyController;
+    def setup(){
+        arena = Mock(Arena.class)
+        Acontrol = new ArenaController(arena)
+        game = Mock(Game.class)
+        key = Mock(KeyStroke.class)
+        time = 5000
+        arenaController = new ArenaController(new Arena(100,60))
+        e_c = Mock(EnemyController.class)
+        h_c  =Mock(HeroController.class)
 
-    public ArenaController(Arena arena) {
-        super(arena);
-
-        this.heroController = new HeroController(arena);
-        this.EnemyController = new EnemyController(arena);
     }
 
-    public void step(Game game, KeyStroke key, long time) throws IOException {
-        if (key == null) {
-            heroController.step(game, key, time);
-            EnemyController.setPosition_hero(getModel().getHero().position);
-            EnemyController.setWalls(getModel().getWalls());
-            EnemyController.step(game, key, time);
-            if(getModel().getHero().getEnergy() <= 0){
-                game.setState(new LoseState(new Lose()));
-            }
+    def 'test step QUIT'(){
+        given:
+        key.getKeyType() >> KeyType.Character ;
+        key.getCharacter() >> 'q';
 
-        } else {
-            if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q') {
-                game.setState(new MenuState(new Menu()));
-                return;
-            }
-            if (this.getModel().getHero().getEnergy() <= 0) {
-                game.setState(new LoseState(new Lose()));
-            }
-            if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'e') {
-                game.setState(null);
-                return;
-            }
-            if (key.getKeyType() == KeyType.Escape) {
-                game.setOldState(game.getState());
-                game.setState(new PauseState(new Pause()));
-            } else {
-                heroController.step(game, key, time);
-                EnemyController.setPosition_hero(getModel().getHero().position);
-                EnemyController.setWalls(getModel().getWalls());
-                EnemyController.step(game, key, time);
-            }
-        }
+        when:
+        Acontrol.step(game, key, time)
+
+        then:
+        1 * game.setState(_)
     }
 
-    public void setEnemyController(EnemyController e){
-        EnemyController = e;
+    def 'test step EXIT'(){
+        given:
+        key.getKeyType() >> KeyType.Character;
+        key.getCharacter() >> 'e';
+
+        when:
+        arenaController.step(game, key, time)
+
+        then:
+        1 * game.setState(_)
     }
-    public void setHeroController(HeroController h){
-        heroController = h;
+
+    def 'test step < erergy'(){
+        given:
+        key.getKeyType() >> KeyType.Character;
+        key.getCharacter() >> 'x';
+        arenaController.getModel().getHero().setEnergy(-10);
+
+        when:
+        arenaController.step(game, key, time)
+
+        then:
+        1 * game.setState(_)
+    }
+
+    def 'test step Escape'(){
+        given:
+        key.getKeyType() >> KeyType.Escape ;
+
+        when:
+        arenaController.step(game, key, time)
+
+        then:
+        1 * game.setState(_)
+        1 * game.setOldState(_)
+    }
+
+    def 'test key = null'(){
+        given:
+        key = null
+        arenaController.setEnemyController(e_c)
+        arenaController.setHeroController(h_c)
+
+        when:
+        arenaController.step(game, key, time)
+
+        then:
+        1 * h_c.step(game, key, time);
+        1 * e_c.setPosition_hero(_);
+        1 * e_c.setWalls(_);
+        1 * e_c.step(game, key, time);
+    }
+
+    def 'test key = null + < energy'(){
+        given:
+        key = null
+        arenaController.setEnemyController(e_c)
+        arenaController.setHeroController(h_c)
+        arenaController.getModel().getHero().setEnergy(-10)
+
+        when:
+        arenaController.step(game, key, time)
+
+        then:
+        1 * h_c.step(game, key, time);
+        1 * e_c.setPosition_hero(_);
+        1 * e_c.setWalls(_);
+        1 * e_c.step(game, key, time);
+        1 * game.setState(_)
+    }
+
+    def 'test key == xxx'(){
+        given:
+        key.getKeyType() >> KeyType.Character ;
+        key.getCharacter() >> 'x';
+        arenaController.setEnemyController(e_c)
+        arenaController.setHeroController(h_c)
+        arenaController.getModel().getHero().setEnergy(-10)
+
+        when:
+        arenaController.step(game, key, time)
+
+        then:
+        1 * h_c.step(game, key, time);
+        1 * e_c.setPosition_hero(_);
+        1 * e_c.setWalls(_);
+        1 * e_c.step(game, key, time);
     }
 }
