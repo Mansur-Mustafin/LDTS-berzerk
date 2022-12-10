28f49bab0adb9093885e81fc1bc0fbfed76e98b7
RafaelNTeixeira <up2021088831@up.pt>
2022-12-10 09:28:06 +0000

    Fixed game text

--- src/main/java/com/RafaelNTeixeira/projeto/viewer/menuViewer/AddLeaderViewer.java
+++ src/main/java/com/RafaelNTeixeira/projeto/viewer/menuViewer/AddLeaderViewer.java
@@ -11,8 +11,8 @@ public class AddLeaderViewer extends Viewer<AddLeader> {
 
     @Override
     public void drawElements(GUI gui) {
-        gui.drawText(new Position(5,12 ), "Enter your name:", "#FFFFFF");
-        gui.drawText(new Position(5, 10), "Your Score: " + getModel().getScore(), "#FFFFFF");
+        gui.drawText(new Position(5,12 ), "enter your name:", "#FFFFFF");
+        gui.drawText(new Position(5, 10), "your Score: " + getModel().getScore(), "#FFFFFF");
 
         String name = getModel().getName();
         gui.drawText(new Position(22, 12), name, "#FFFFFF");
--- src/main/java/com/RafaelNTeixeira/projeto/viewer/menuViewer/InstructionViewer.java
+++ src/main/java/com/RafaelNTeixeira/projeto/viewer/menuViewer/InstructionViewer.java
@@ -11,23 +11,26 @@ public class InstructionViewer extends Viewer<Instruction> {
 
     @Override
     public void drawElements(GUI gui) {
-        gui.drawText(new Position(0, 3), "[]=============================[]", "#E9FFDB");
-        gui.drawText(new Position(0, 4), "||   I N S T R U C T I O N S   || ", "#E9FFDB");
-        gui.drawText(new Position(0, 5), "||=============================||", "#E9FFDB");
+        gui.drawText(new Position(0, 1), "[]=============================[]", "#E9FFDB");
+        gui.drawText(new Position(0, 2), "||   i n s t r u c t i o n s   || ", "#E9FFDB");
+        gui.drawText(new Position(0, 3), "||=============================||", "#E9FFDB");
+        gui.drawText(new Position(0, 4), "||                             ||", "#E9FFDB");
+        gui.drawText(new Position(0, 5), "||      gameplay constrols:    ||", "#E9FFDB");
         gui.drawText(new Position(0, 6), "||                             ||", "#E9FFDB");
-        gui.drawText(new Position(0, 7), "||       gameplay CONTROLS:    ||", "#E9FFDB");
-        gui.drawText(new Position(0, 8), "||                             ||", "#E9FFDB");
-        gui.drawText(new Position(0, 9), "||      W --> mOVE UPWARDS     ||", "#E9FFDB");
-        gui.drawText(new Position(0, 10), "||                             ||", "#E9FFDB");
-        gui.drawText(new Position(0, 11), "||      S --> mOVE DOWNWARDS   ||", "#E9FFDB");
-        gui.drawText(new Position(0, 12), "||                             ||", "#E9FFDB");
-        gui.drawText(new Position(0, 13), "||      D --> mOVE RIGHT       ||", "#E9FFDB");
-        gui.drawText(new Position(0, 14), "||                             ||", "#E9FFDB");
-        gui.drawText(new Position(0, 15), "||      A --> mOVE LEFT        ||", "#E9FFDB");
-        gui.drawText(new Position(0, 16), "||                             ||", "#E9FFDB");
-        gui.drawText(new Position(0, 17), "[]=============================[]", "#E9FFDB");
+        gui.drawText(new Position(0, 7), "||                             ||", "#E9FFDB");
+        gui.drawText(new Position(0, 8), "||      w --> move upwards     ||", "#E9FFDB");
+        gui.drawText(new Position(0, 9), "||                             ||", "#E9FFDB");
+        gui.drawText(new Position(0, 10), "||      s --> move downwards   ||", "#E9FFDB");
+        gui.drawText(new Position(0, 11), "||                             ||", "#E9FFDB");
+        gui.drawText(new Position(0, 12), "||      d --> move right       ||", "#E9FFDB");
+        gui.drawText(new Position(0, 13), "||                             ||", "#E9FFDB");
+        gui.drawText(new Position(0, 14), "||      a --> move left        ||", "#E9FFDB");
+        gui.drawText(new Position(0, 15), "||                             ||", "#E9FFDB");
+        gui.drawText(new Position(0, 16), "||    arrow keys --> shoot     ||", "#E9FFDB");
+        gui.drawText(new Position(0, 17), "||                             ||", "#E9FFDB");
+        gui.drawText(new Position(0, 18), "||=============================||", "#E9FFDB");
 
 
-        gui.drawText(new Position(2, 20), "Press Enter to go back to menu", "#FFFFFF");
+        gui.drawText(new Position(2, 21), "Press enter to go back to menu", "#FFFFFF");
     }
 }
