6e74596b73a11a5c3b22ed5284eab1ead9cddef3
Ian <ianbeltrao@gmail.com>
2022-12-11 02:56:07 +0000

    fixed some tests

--- src/test/groovy/com/RafaelNTeixeira/projeto/viewer/gameViewer/BulletViewerTest.groovy
+++ src/test/groovy/com/RafaelNTeixeira/projeto/viewer/gameViewer/BulletViewerTest.groovy
@@ -8,21 +8,40 @@ class BulletViewerTest extends Specification{
 
     private def gui
 
-    def 'Draw bullet'() {
+    def 'Draw hero bullet'() {
         given:
         boolean t = true
-        boolean f = false
+
         Bullet hero_bullet = new Bullet(10, 10, 'r' as char, t)
-        Bullet enemy_bullet = new Bullet(10, 10, 'l' as char, f)
+
         gui = Mock(GUILaterna.class)
         BulletViewer bulletViewer = new BulletViewer()
 
         when:
         bulletViewer.draw(hero_bullet, gui)
-        bulletViewer.draw(enemy_bullet, gui)
+
 
         then:
         1*gui.drawHeroBullet(_)
+
+
+    }
+
+    def 'Draw enemy bullet'() {
+        given:
+
+        boolean f = false
+
+        Bullet enemy_bullet = new Bullet(10, 10, 'l' as char, f)
+        gui = Mock(GUILaterna.class)
+        BulletViewer bulletViewer = new BulletViewer()
+
+        when:
+
+        bulletViewer.draw(enemy_bullet, gui)
+
+        then:
+
         1*gui.drawEnemyBullet(_)
 
     }
